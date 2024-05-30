a406e66aa70565d72d95116671cedea76826c3c0
thevijeeth@gmail.com <thevijeeth@gmail.com>
2024-05-30 22:03:07 +0530

    v1

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function() {
+  lottie.loadAnimation({
+    container: document.getElementById('animation1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './animation1.json'
+  });
+
+  lottie.loadAnimation({
+    container: document.getElementById('animation2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './animation2.json'
+  });
+
+  lottie.loadAnimation({
+    container: document.getElementById('animation3'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './animation3.json'
+  });
+
+  lottie.loadAnimation({
+    container: document.getElementById('animation4'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './animation4.json'
+  });
+
+  lottie.loadAnimation({
+    container: document.getElementById('animation5'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './animation5.json'
+  });
+
+  lottie.loadAnimation({
+    container: document.getElementById('animation6'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './animation6.json'
+  });
+});
